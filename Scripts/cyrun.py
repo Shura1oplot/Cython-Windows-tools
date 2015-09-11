79eda1af92d247c10b596fed3e492bb4cf3554cb
Alexander Gordeyev <s0meuser@yandex.ru>
2015-09-11 10:42:40 +0300

    update

--- Scripts/cyrun.py
+++ Scripts/cyrun.py
@@ -8,26 +8,40 @@ from __future__ import print_function
 import sys
 import os
 import io
+import locale
+import time
+import shutil
 
-from dufilib import with_timer
 from cypp import run as cypp_run
 
 
+reload(sys)
+sys.setdefaultencoding(locale.getpreferredencoding())
+
+
 def nt(a, b):
-    if not os.path.exists(b):
+    if os.path.exists(a):
+        if os.path.exists(b):
+            return os.stat(a).st_mtime > os.stat(b).st_mtime
+
         return True
 
-    return os.stat(a).st_mtime > os.stat(b).st_mtime
+    elif os.path.exists(b):
+        return False
+
+    else:
+        raise ValueError("both files do not exist")
 
 
 def update_pyx(py_file, pyx_file):
     fpi = io.open(py_file, "r", encoding="utf-8")
     fpo = io.open(pyx_file, "w", encoding="utf-8")
 
-    cypp_run(fpi, fpo)
-
-    fpi.close()
-    fpo.close()
+    try:
+        cypp_run(fpi, fpo)
+    finally:
+        fpi.close()
+        fpo.close()
 
 
 def update_pyd(pyx_file):
@@ -40,7 +54,8 @@ def update_pyd(pyx_file):
     try:
         setup(ext_modules=cythonize(pyx_file))
     except SystemExit as e:
-        print(e)
+        if e.code != 0:
+            raise
 
     sys.argv[:] = argv
 
@@ -49,17 +64,49 @@ def run_pyd(pyd_file, argv):
     fname, _ = os.path.splitext(os.path.basename(pyd_file))
     sys.path.insert(0, os.path.dirname(pyd_file))
     module = __import__(fname)
-    return with_timer(module.main)([pyd_file, ] + argv)
+
+    print(">" * 80)
+
+    ts = time.time()
+    rv = module.main([pyd_file, ] + argv)
+    td = time.time() - ts
+
+    print("<" * 80)
+    print("time: {}h {}m {:.2f}s".format(int(td / 60 / 60),
+                                         int((td / 60) % 60),
+                                         td % 60),
+          file=sys.stderr)
+
+    return rv
+
+
+def clean(pyx_file, pyd_file=None):
+    if os.path.isfile(pyx_file):
+        os.remove(pyx_file)
+
+    if pyx_file.endswith(".pyx"):
+        c_file = pyx_file[:-3] + "c"
+
+        if os.path.isfile(c_file):
+            os.remove(c_file)
+
+    if pyd_file and os.path.isfile(pyd_file):
+        os.remove(pyd_file)
+
+    if os.path.isdir("build"):
+        shutil.rmtree("build")
 
 
 def main(argv=sys.argv):
+    prog = os.path.basename(argv[0])
+
     try:
         py_file = argv[1]
     except IndexError:
         return 0
 
     if not os.path.isfile(py_file):
-        print("cannot open file: {}".format(py_file))
+        print("{}: cannot open file: {}".format(prog, py_file))
         return 1
 
     if py_file.endswith(b".py"):
@@ -69,14 +116,11 @@ def main(argv=sys.argv):
 
     pyd_file = b"{}d".format(pyx_file[:-1])
 
-    if nt(py_file, pyx_file):
+    if nt(py_file, pyd_file):
+        clean(pyx_file, pyd_file)
         update_pyx(py_file, pyx_file)
-
-    if nt(pyx_file, pyd_file):
-        if os.path.isfile(pyd_file):
-            os.remove(pyd_file)
-
         update_pyd(pyx_file)
+        clean(pyx_file)
 
     if os.path.isfile(pyd_file):
         return run_pyd(pyd_file, argv[2:])
